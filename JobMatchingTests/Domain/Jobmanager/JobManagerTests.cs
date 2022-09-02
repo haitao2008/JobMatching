72c63fa8ed31650e740eccf5b443bf2b4acc576d
Tom Zhao <tom.zhao@genea.com.au>
2022-09-02 23:09:41 +1000

    Added another test cases to find the most matched candidate for job1.

--- JobMatchingTests/Domain/Jobmanager/JobManagerTests.cs
+++ JobMatchingTests/Domain/Jobmanager/JobManagerTests.cs
@@ -24,5 +24,21 @@ namespace JobMatching.Domain.Jobmanager.Tests
             var candidates = JobManager.Get_All_Candidates();
             Assert.AreNotEqual(0, candidates.Count);
         }
+
+        [TestMethod()]
+        public void Get_The_Most_Matched_Candidate_Test()
+        {
+            var candidates = JobManager.Get_All_Candidates();
+            var job = JobManager.Get_All_Jobs().First(x => x.JobId == 1);
+
+            var matchedTags = candidates
+                .Select(x => new { Count = x.Skill_Tag.Intersect(job.Skill_Tag).Count(), Id = x.CandidateId })
+                .Max(x => x.Count);
+
+            var mostMatchedCandidate = candidates.MaxBy(x => x.Skill_Tag.Intersect(job.Skill_Tag).Count());
+            Assert.AreEqual(3, matchedTags);
+            Assert.AreEqual(3, mostMatchedCandidate.CandidateId);
+        }
+
     }
 }
