d806d9a2a62e98bd860ea3db0610be295207a137
Tom Zhao <tom.zhao@genea.com.au>
2022-09-02 22:01:07 +1000

    Add project files.

---
+++ JobMatching/Domain/Jobmanager/JobManager.cs
@@ -0,0 +1,6 @@
+﻿namespace JobMatching.Domain.Jobmanager
+{
+    public class JobManager
+    {
+    }
+}
---
+++ JobMatching/Domain/Models/Job.cs
@@ -0,0 +1,22 @@
+﻿namespace JobMatching.Domain.Models
+{
+    public class Job
+    {
+        public int JobId { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Skills { get; set; }
+
+        public List<string> Skill_Tag
+        {
+            get 
+            {
+                if(!string.IsNullOrEmpty(Skills))
+                {
+                    return Skills.Split(",", StringSplitOptions.TrimEntries).ToList();
+                }
+                return new List<string>();
+            }
+        }
+    }
+}
